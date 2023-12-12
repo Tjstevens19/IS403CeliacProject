798093ca334b1d66152766bfc074031fabf0d524
Tjstevens19 <tdogstrike@gmail.com>
2023-12-12 15:48:56 -0700

    add .platform/hooks/postdeploy/00_get_certificate.sh

---
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Place in .platform/hooks/postdeploy directory
+sudo certbot -n -d project3-3nv.eba-shpct4mm.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email ethann55@byu.edu
